4848aa575a16207280eeac48e558e6c030203ed2
GamerMax1254 <maxfirefly@yandex.ru>
2024-12-31 12:49:57 +0300

    Add files via upload

---
+++ TimeToNewYear/TimeToNewYear/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CountdownToNewYear
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Время до Нового года:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(7, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(448, 69);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0д 0ч 0м 0с";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 121);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Time to New Year";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TimeToNewYear/TimeToNewYear/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CountdownToNewYear
+{
+    public partial class Form1 : Form
+    {
+        DateTime newYear;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCountdown();
+            UpdateLabel();
+            Timer timer = new Timer();
+            timer.Interval = 1000; // Обновление каждую секунду
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void InitializeCountdown()
+        {
+            // Устанавливаем дату Нового года
+            newYear = new DateTime(DateTime.Now.Year + 1, 1, 1);
+        }
+
+        private void UpdateLabel()
+        {
+            // Получаем текущую дату и время
+            DateTime now = DateTime.Now;
+
+            // Вычисляем разницу в времени
+            TimeSpan timeUntilNewYear = newYear - now;
+
+            // Формируем строку для вывода
+            string countdownString = $"{timeUntilNewYear.Days} д {timeUntilNewYear.Hours} ч " +
+                                      $"{timeUntilNewYear.Minutes} м {timeUntilNewYear.Seconds} с";
+
+            // Обновляем текст label2
+            label2.Text = countdownString;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateLabel();
+        }
+    }
+}
